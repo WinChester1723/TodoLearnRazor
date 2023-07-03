d8586e7908225c4677968fd1f1cd0ee47430d063
Pasha Pashazade <qordon23@gmail.com>
2023-07-03 14:09:27 +0400

    Add project files.

---
+++ src/RazorPagesLearn/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace RazorPagesLearn.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
